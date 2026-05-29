7018239e0496125e5fa24e80e5f3b76ac523428c
Mutahir Bin Athar <164089040+Mutahir-15@users.noreply.github.com>
2026-05-29 23:37:02 +0500

    Applied fixes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,19 @@
-// TODO: Implement Home page
+import React from 'react';
+import { HeroSection, AboutSection } from '@/components/sections';
+
+/**
+ * Portfolio Home Page
+ * Renders the primary landing sections in sequence.
+ */
 export default function Home() {
   return (
-    <main>
-      <h1>Portfolio</h1>
-    </main>
+    <div className="flex flex-col w-full">
+      <HeroSection />
+      <AboutSection />
+      {/* 
+        Other sections (Skills, Projects, etc.) will be added 
+        here as they are fully implemented in Phase 2.
+      */}
+    </div>
   );
 }
